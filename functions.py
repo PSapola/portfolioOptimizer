a0bdb24093dffcfa1adba157136e847797dbb9d6
Paulius <73410466+PSapola@users.noreply.github.com>
2025-10-19 18:44:05 +0300

    added function to calculate the sharpe-ratio

--- functions.py
+++ functions.py
@@ -25,3 +25,8 @@ def portfolio_performance(weights, mean_returns, cov_matrix):
     portfolio_return = np.sum(weights*mean_returns) #calculate portfolio returns
     portfolio_volatility = np.sqrt(np.dot(weights.transpose(),np.dot(cov_matrix,weights))) #calculate portfolio volatility
     return portfolio_return, portfolio_volatility
+
+def negative_sharpe(weights, mean_returns, cov_matrix, risk_free_rate = 0.02): 
+    p_return, p_vol = portfolio_performance(weights, mean_returns, cov_matrix) 
+    sharpe = (p_return-risk_free_rate)/p_vol #formula for sharpe ratio
+    return -sharpe # numpy doesn't have maximize(), returning -sharpe to find the best portfolio
