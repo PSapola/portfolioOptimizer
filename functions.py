058364ba153914b8ef0e27f598d68a031b8898fc
Paulius <73410466+PSapola@users.noreply.github.com>
2025-10-19 19:50:30 +0300

    fixed file structure, made it possible for the user to choose stocks

--- functions.py
+++ functions.py
@@ -1,37 +1,25 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd
-import matplotlib as plt
-from scipy.optimize import minimize
 
 
-tickers = ['AAPL', 'MSFT', 'GOOGL', 'AVGO', 'UNH']
-print('Downloading data...')
-price_data = {}
 
-for ticker in tickers:
-    get_stock_data = yf.download(ticker,start='2020-01-01',end='2025-10-19',progress=False,auto_adjust=False)
-    price_data[ticker] = get_stock_data['Close'].squeeze() #.squeeze() from DataFrame to series
+def get_tickers():
+    ticker_amount = int(input('Enter the amount of stocks your portfolio will have: '))
+    tickers = []
+    for _ in range(ticker_amount):
+        user_ticker = input('Enter a ticker: ').upper().strip()
+        tickers.append(user_ticker)
+    return tickers
 
-data = pd.DataFrame(price_data)
-returns = data.pct_change().dropna() 
-
-mean_returns = returns.mean() * 252 #252 trading days p.y.
-cov_matrix = returns.cov() * 252 
 
 def portfolio_performance(weights, mean_returns, cov_matrix):
     portfolio_return = np.sum(weights*mean_returns) #calculate portfolio returns
     portfolio_volatility = np.sqrt(np.dot(weights.transpose(),np.dot(cov_matrix,weights))) #calculate portfolio volatility
     return portfolio_return, portfolio_volatility
 
+
 def negative_sharpe(weights, mean_returns, cov_matrix, risk_free_rate = 0.02): 
     p_return, p_vol = portfolio_performance(weights, mean_returns, cov_matrix) 
     sharpe = (p_return-risk_free_rate)/p_vol #formula for sharpe ratio
     return -sharpe # scipy doesn't have maximize(), returning -sharpe to find the best portfolio
-
-num_assets = len(tickers) 
-initial_guess = num_assets * [1/num_assets] #initial weight guess, even split between the number of stocks
-constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1} # the sum of all constraints - 1 MUST equal 0, if not, we have "money" sitting uninvested 
-bounds = tuple((0,0.33) for _ in range(num_assets)) #the potential weights for each stock. NO short selling (no negative weights)
-
-result = minimize(negative_sharpe,initial_guess,args=(mean_returns,cov_matrix),method='SLSQP',bounds=bounds,constraints=constraints) #calculate optimal weights and sharpe for those weights
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from functions import *
+from scipy.optimize import minimize
+
+
+tickers = get_tickers()
+price_data = {}
+
+print('Downloading data...')
+
+for ticker in tickers:
+    get_stock_data = yf.download(ticker,start='2020-01-01',end='2025-10-19',progress=False,auto_adjust=False)
+    price_data[ticker] = get_stock_data['Close'].squeeze() #.squeeze() from DataFrame to series
+
+data = pd.DataFrame(price_data)
+returns = data.pct_change().dropna() 
+
+
+num_assets = len(tickers) 
+initial_guess = num_assets * [1/num_assets] #initial weight guess, even split between the number of stocks
+constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1} # the sum of all constraints - 1 MUST equal 0, if not, we have "money" sitting uninvested 
+bounds = tuple((0,0.33) for _ in range(num_assets)) #the potential weights for each stock. NO short selling (no negative weights)
+
+
+mean_returns = returns.mean() * 252 #252 trading days p.y.
+cov_matrix = returns.cov() * 252 
+
+result = minimize(negative_sharpe,initial_guess,args=(mean_returns,cov_matrix),method='SLSQP',bounds=bounds,constraints=constraints) #calculate optimal weights and sharpe for those weights
 
 optimal_weights = result.x
 
