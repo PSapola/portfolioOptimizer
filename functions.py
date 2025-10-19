7996fbde6bfc1acf77d52330a66daa88af8e5637
Paulius <73410466+PSapola@users.noreply.github.com>
2025-10-19 17:16:06 +0300

    added stock data functionality

--- functions.py
+++ functions.py
@@ -3,3 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 from scipy.optimize import minimize
+
+
+tickers = ['AAPL', 'MSFT', 'GOOGL', 'AVGO', 'UNH']
+price_data = {}
+
+for ticker in tickers:
+    get_stock_data = yf.download(ticker,start='2020-01-01',end='2025-10-19')
+    price_data[ticker] = get_stock_data['Close']
