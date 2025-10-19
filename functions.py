96e87f88eceb3b544a0a55d99abf9ef73f6972cf
Paulius <73410466+PSapola@users.noreply.github.com>
2025-10-19 19:12:42 +0300

    added main

--- functions.py
+++ functions.py
@@ -38,16 +38,3 @@ bounds = tuple((0,1) for _ in range(num_assets)) #the potential weights for each
 
 result = minimize(negative_sharpe,initial_guess,args=(mean_returns,cov_matrix),method='SLSQP',bounds=bounds,constraints=constraints) #calculate optimal weights and sharpe for those weights
 #print(result)
-
-optimal_weights = result.x
-
-opt_return, opt_vol = portfolio_performance(weights=optimal_weights,mean_returns=mean_returns,cov_matrix=cov_matrix) # calculating the optimal returns and volatility and unpacking directly 
-opt_sharpe = (opt_return-0.02)/opt_vol # new optimal sharpe with opt weights
-
-print("=== OPTIMAL PORTFOLIO ===")
-for ticker, weight in zip(tickers, optimal_weights):
-    print(f"{ticker}: {weight*100:.2f}%")
-
-print(f"\nExpected Annual Return: {opt_return*100:.2f}%")
-print(f"Annual Volatility: {opt_vol*100:.2f}%")
-print(f"Sharpe Ratio: {opt_sharpe:.3f}")
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import functions
+from functions import *
+
+optimal_weights = result.x
+
+opt_return, opt_vol = portfolio_performance(weights=optimal_weights,mean_returns=mean_returns,cov_matrix=cov_matrix) # calculating the optimal returns and volatility and unpacking directly 
+opt_sharpe = (opt_return-0.02)/opt_vol # new optimal sharpe with opt weights
+
+print("=== OPTIMAL PORTFOLIO ===")
+for ticker, weight in zip(tickers, optimal_weights):
+    print(f"{ticker}: {weight*100:.2f}%")
+
+print(f"\nExpected Annual Return: {opt_return*100:.2f}%")
+print(f"Annual Volatility: {opt_vol*100:.2f}%")
+print(f"Sharpe Ratio: {opt_sharpe:.3f}")
