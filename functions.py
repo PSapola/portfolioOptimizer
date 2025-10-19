59453e70c5ba55ba7f8670bfd6bac4b09368b31f
Paulius <73410466+PSapola@users.noreply.github.com>
2025-10-19 18:01:45 +0300

    reduced clutter in terminal

--- functions.py
+++ functions.py
@@ -6,11 +6,14 @@ from scipy.optimize import minimize
 
 
 tickers = ['AAPL', 'MSFT', 'GOOGL', 'AVGO', 'UNH']
+print('Downloading data...')
 price_data = {}
 
 for ticker in tickers:
-    get_stock_data = yf.download(ticker,start='2020-01-01',end='2025-10-19',progress=False)
-    price_data[ticker] = get_stock_data['Close'].squeeze()
+    get_stock_data = yf.download(ticker,start='2020-01-01',end='2025-10-19',progress=False,auto_adjust=False)
+    price_data[ticker] = get_stock_data['Close'].squeeze() #.squeeze() from DataFrame to series
 
 data = pd.DataFrame(price_data)
 print(data.head())
+
+returns = data.pct_change().dropna() 
