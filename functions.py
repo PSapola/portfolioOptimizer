0eb4ac74437b864d39f1f80c8c38dd3b2ebeed0a
Paulius <73410466+PSapola@users.noreply.github.com>
2025-10-19 19:10:07 +0300

    cleaned up the data

--- functions.py
+++ functions.py
@@ -37,4 +37,17 @@ constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1} # the sum of all co
 bounds = tuple((0,1) for _ in range(num_assets)) #the potential weights for each stock. NO short selling (no negative weights)
 
 result = minimize(negative_sharpe,initial_guess,args=(mean_returns,cov_matrix),method='SLSQP',bounds=bounds,constraints=constraints) #calculate optimal weights and sharpe for those weights
-print(result)
+#print(result)
+
+optimal_weights = result.x
+
+opt_return, opt_vol = portfolio_performance(weights=optimal_weights,mean_returns=mean_returns,cov_matrix=cov_matrix) # calculating the optimal returns and volatility and unpacking directly 
+opt_sharpe = (opt_return-0.02)/opt_vol # new optimal sharpe with opt weights
+
+print("=== OPTIMAL PORTFOLIO ===")
+for ticker, weight in zip(tickers, optimal_weights):
+    print(f"{ticker}: {weight*100:.2f}%")
+
+print(f"\nExpected Annual Return: {opt_return*100:.2f}%")
+print(f"Annual Volatility: {opt_vol*100:.2f}%")
+print(f"Sharpe Ratio: {opt_sharpe:.3f}")
